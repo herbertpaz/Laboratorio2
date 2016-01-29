c9ae6e23bcd3f1b6ee9e133303c19694adac10a5
Herbert.paz <herbert.paz@unitec.edu>
2016-01-29 16:46:11 -0600

    Ya termine las validaciones

--- laboratorio2.cpp
+++ laboratorio2.cpp
@@ -45,8 +45,58 @@ int main(int argc, char*argv[]){
                 cin>>x4;
                 cout<<"y: ";
                 cin>>y4;
-		while(){
+		int validadorx=0,validadory=0;
+		if((x1==x2)||(x1==x3)||(x1==x4)){
+			cout<<"prueba";
+			validadorx+=1;
+		}	
+		if((x2==x1)||(x2==x3)||(x2==x4)){
+                        validadorx+=1;
+                }	
+		if((x3==x1)||(x3==x2)||(x3==x4)){
+                        validadorx+=1;
+                }
+		if((x4==x1)||(x4==x2)||(x4==x3)){
+                        validadorx+=1;
+                }
+		if((y1==y2)||(y1==y3)||(y1==y4)){
+                        validadory+=1;
+                }
+		if((y2==y1)||(y2==y3)||(y2==y4)){
+                        validadory+=1;
+                }
+		if((y3==y1)||(y3==y2)||(y3==y4)){
+                        validadory+=1;
+                }
+		if((y4==y1)||(y4==y2)||(y4==y3)){
+                        validadory+=1;
+                }
+		if((validadorx<=2)&&(validadory<=2)){
+			cout<<"hola";
+			int validador=0;
+			if(((x1==x2)&&(y1==y2))||((x1==x3)&&(y1==y3))||((x1==x4)&&(y1==y4))){
+				validador=1;
+			}
+			if(((x2==x1)&&(y2==y1))||((x2==x3)&&(y2==y3))||((x2==x4)&&(y2==y4))){
+                                validador=1;
+                        }
+			if(((x3==x1)&&(y3==y1))||((x3==x2)&&(y3==y2))||((x3==x4)&&(y3==y4))){
+                                validador=1;
+                        }
+			if(((x4==x1)&&(y4==y1))||((x4==x2)&&(y4==y2))||((x4==x3)&&(y4==y3))){
+                                validador=1;
+                        }
+			if(validador==0){
+				cout<<"hola2";
+			//aqui comienza
+			}else{
+				cout<<"Ingreso puntos repetidos";
+			}
+
+		}else{
+			cout<<"Los puntos que ingreso no forman un trapezoide";
 		}
+
 	}else{
 		cout<<"Opcion no valida";
 	}		
