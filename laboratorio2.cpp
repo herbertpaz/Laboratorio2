5c7540ba384ca8acc68673c559beae29978fcd9f
Herbert.paz <herbert.paz@unitec.edu>
2016-01-29 18:14:20 -0600

    Ya termine el laboratorio 2

--- laboratorio2.cpp
+++ laboratorio2.cpp
@@ -53,29 +53,26 @@ int main(int argc, char*argv[]){
                 cin>>y4;
 		int validadorx=0,validadory=0;
 		if((x1==x2)||(x1==x3)||(x1==x4)){
-			validadorx+=1;
+			validadorx+=1;	
+			cout<<"1";
 		}	
-		if((x2==x1)||(x2==x3)||(x2==x4)){
+		if((x2==x3)||(x2==x4)){
                         validadorx+=1;
+			cout<<"2";
                 }	
-		if((x3==x1)||(x3==x2)||(x3==x4)){
+		if(x3==x4){
                         validadorx+=1;
-                }
-		if((x4==x1)||(x4==x2)||(x4==x3)){
-                        validadorx+=1;
+			cout<<"3";
                 }
 		if((y1==y2)||(y1==y3)||(y1==y4)){
                         validadory+=1;
                 }
-		if((y2==y1)||(y2==y3)||(y2==y4)){
+		if((y2==y3)||(y2==y4)){
                         validadory+=1;
                 }
-		if((y3==y1)||(y3==y2)||(y3==y4)){
+		if(y3==y4){
                         validadory+=1;
-                }
-		if((y4==y1)||(y4==y2)||(y4==y3)){
-                        validadory+=1;
-                }
+               	}
 		if((validadorx<=2)&&(validadory<=2)){
 			int validador=0;
 			if(((x1==x2)&&(y1==y2))||((x1==x3)&&(y1==y3))||((x1==x4)&&(y1==y4))){
@@ -92,6 +89,53 @@ int main(int argc, char*argv[]){
                         }
 			if(validador==0){
 			//aqui comienza
+				cout<<"Los puntos son:"<<endl;
+				cout<<"( "<<x1<<", "<<y1<<")"<<endl;
+				cout<<"( "<<x2<<", "<<y2<<")"<<endl;
+				cout<<"( "<<x3<<", "<<y3<<")"<<endl;
+				cout<<"( "<<x4<<", "<<y4<<")"<<endl;
+				cout<<endl;
+				cout<<"Los lados del trapezoide son:"<<endl;
+				cout<<"Lado 1: "<<recta(x1,x2,y1,y2)<<endl;
+				cout<<"Lado 2: "<<recta(x3,x1,y3,y1)<<endl;
+				cout<<"Lado 3: "<<recta(x3,x4,y3,y4)<<endl;
+				cout<<"Lado 4: "<<recta(x2,x4,y2,y4)<<endl;
+				cout<<endl;
+				cout<<"Los lados del triangulo 1 miden: "<<endl;
+				cout<<"Lado 1: "<<recta(x3,x1,y3,y1)<<endl;
+				double a1=recta(x3,x1,y3,y1);
+				cout<<"Lado 2: "<<recta(x3,x4,y3,y4)<<endl;
+				double b1=recta(x3,x4,y3,y4);
+				cout<<"Lado 3: "<<recta(x1,x4,y1,y4)<<endl;
+				double c1=recta(x1,x4,y1,y4);
+				cout<<"Los lados del triangulo 2 miden: "<<endl;
+                                cout<<"Lado 1: "<<recta(x1,x2,y1,y2)<<endl;
+				double a2=recta(x1,x2,y1,y2);
+                                cout<<"Lado 2: "<<recta(x2,x4,y2,y4)<<endl;
+				double b2=recta(x2,x4,y2,y4);
+                                cout<<"Lado 3: "<<recta(x1,x4,y1,y4)<<endl;
+				double c2=recta(x1,x4,y1,y4);
+				cout<<endl;	
+				double semi1= semi(a1,b1,c1);
+				double semi2= semi(a2,b2,c2);
+				cout<<"Semiperimetro del triangulo 1: "<<semi1<<endl;
+				cout<<"Semiperimetro del triangulo 2: "<<semi2<<endl;
+				cout<<"Perimetro del triangulo 1: "<< a1+b1+c1<<endl;
+				cout<<"Perimetro del triangulo 2: "<< a2+b2+c2<<endl;
+				cout<<endl;
+				cout<<"Las alturas del triangulo 1 es: "<<endl;
+				cout<<"Altura 1: "<<alturaa(a1,b1,c1)<<endl;
+				cout<<"Altura 2: "<<alturab(a1,b1,c1)<<endl;
+				cout<<"Altura 3: "<<alturac(a1,b1,c1)<<endl;
+				double h1=alturac(a1,b1,c1);
+				cout<<endl;
+				cout<<"Las alturas del triangulo 2 es: "<<endl;          
+                                cout<<"Altura 1: "<<alturaa(a2,b2,c2)<<endl;
+                                cout<<"Altura 2: "<<alturab(a2,b2,c2)<<endl;
+                                cout<<"Altura 3: "<<alturac(a2,b2,c2)<<endl;
+				double h2=alturac(a2,b2,c2);
+                                cout<<endl;				
+				cout<<"El area del trapezoide: "<<area(c1,h1,h2)<<endl;
 			}else{
 				cout<<"Ingreso puntos repetidos"<<endl;
 			}
@@ -123,6 +167,7 @@ double recta(double x1,double x2,double y1,double y2){
 double semi(double a,double b,double c){
 	double valor;
 	valor= (a+b+c)/2.0;
+	return valor;
 }
 
 double alturaa(double a,double b,double c){
